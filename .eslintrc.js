2d54f877c8e66d2c708dcb45ab48a64e8ae70216
Iris <irinaais90@yandex.ru>
2022-07-26 01:58:32 +0300

    feat: editorconfig, eslint, git, nodemon configured. Add app.js with express-server. Connected to the server MongoDB (mongodb://localhost:27017/mestodb)

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+     "allow": ["_id"],
+  },
+};
