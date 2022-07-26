89fa75d763dd0e350337d8bf73452c7f438e5c45
Iris <irinaais90@yandex.ru>
2022-07-26 23:07:28 +0300

    feat: add routes and controllers for users

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,10 @@
 const router = require('express').Router();
 // const { users } = require ('../usersDB');
-const User = require ('../models/user');
+const { sendUsers, sendUserById, createUser } = require('../controllers/users');
 
-router.get('/users', (req, res) => {
-  User.find({})
-    .then(users => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка'}));
-})
-
-router.get('/users/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка'}));
-})
-
-router.post('/users', (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then(user => res.send({ data: user}))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка'}));
-})
+router.get('/users', sendUsers);
+router.get('/users/:userId', sendUserById);
+router.post('/users', createUser);
 
 module.exports = router;
 
