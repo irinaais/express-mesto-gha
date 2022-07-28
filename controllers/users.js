47a09a5e7d0bf731b80c49522bf53d7f678077b8
Iris <irinaais90@yandex.ru>
2022-07-29 00:56:45 +0300

    fix: controller sendUserById

--- controllers/users.js
+++ controllers/users.js
@@ -10,9 +10,9 @@ module.exports.sendUsers = (req, res) => {
 };
 
 module.exports.sendUserById = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((user) => {
-      if (!user._id) {
+      if (!user) {
         res.status(NOT_FOUND_CODE).send({ message: 'Указанный пользователь не найден'})
         return;
       }
@@ -44,7 +44,7 @@ module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => {
-      if (!user._id) {
+      if (!user) {
         res.status(NOT_FOUND_CODE).send({ message: 'Указанный пользователь не найден'})
         return;
       }
@@ -63,7 +63,7 @@ module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => {
-      if (!user._id) {
+      if (!user) {
         res.status(NOT_FOUND_CODE).send({ message: 'Указанный пользователь не найден'})
         return;
       }
