7a2d3147497f64f75beda85b3d202f4961688883
Iris <irinaais90@yandex.ru>
2022-08-06 10:41:28 +1000

    fix: auth, reading cookies

--- controllers/users.js
+++ controllers/users.js
@@ -104,7 +104,8 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const key = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+      const token = jwt.sign({ _id: user._id }, key, { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,15 +7,20 @@ const handleAuthError = (res) => {
   res.status(AUTH_ERROR_CODE).send({ message: 'Необходима авторизация' });
 };
 
-const extractBearerToken = (header) => header.replace('Bearer', '');
+const extractBearerToken = (header) => header.replace('Bearer ', '');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer')) {
+  let token;
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = extractBearerToken(authorization);
+  } else if (req.cookies.jwt != null) {
+    token = req.cookies.jwt;
+  } else {
     handleAuthError(res);
     return;
   }
-  const token = extractBearerToken(authorization);
+
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
