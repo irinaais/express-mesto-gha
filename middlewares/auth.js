8cb65cb329995730bdd1ab87df1c6b40dc8f09e3
Iris <irinaais90@yandex.ru>
2022-08-10 01:03:47 +1000

    feat: restored authorization with header and cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,49 +7,48 @@ const handleAuthError = (res) => {
   res.status(AUTH_ERROR_CODE).send({ message: 'Необходима авторизация' });
 };
 
-module.exports = (req, res, next) => {
-  const authorization = req.cookies.jwt;
-
-  if (!authorization) {
-    handleAuthError(res);
-  } else {
-    const token = authorization;
-    let payload;
-    try {
-      payload = jwt.verify(token, 'dev-secret'); // JWT_SECRET);
-    } catch (err) {
-      console.log(err);
-      handleAuthError(res);
-      return;
-    }
-    req.user = payload;
-    next();
-  }
-};
-// const extractBearerToken = (header) => header.substring(7);
-
 // module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   let token;
+//   const authorization = req.cookies.jwt;
 //
-//   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-//     token = extractBearerToken(authorization);
-//   } else if (req.cookies.jwt != null) {
-//     token = req.cookies.jwt;
+//   if (!authorization) {
+//     handleAuthError(res);
 //   } else {
-//     handleAuthError(res);
-//     return;
+//     const token = authorization;
+//     let payload;
+//     try {
+//       payload = jwt.verify(token, 'dev-secret'); // JWT_SECRET);
+//     } catch (err) {
+//       console.log(err);
+//       handleAuthError(res);
+//       return;
+//     }
+//     req.user = payload;
+//     next();
 //   }
-//   // console.log(`JWT_SECRET=${JWT_SECRET}`);
-//   // console.log(`token=${token}`);
-//   let payload;
-//   try {
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     console.log(err);
-//     handleAuthError(res);
-//     return;
-//   }
-//   req.user = payload;
-//   next();
 // };
+
+const extractBearerToken = (header) => header.substring(7);
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  let token;
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    token = extractBearerToken(authorization);
+  } else if (req.cookies.jwt != null) {
+    token = req.cookies.jwt;
+  } else {
+    handleAuthError(res);
+    return;
+  }
+  let payload;
+  try {
+    payload = jwt.verify(token, 'dev-secret');
+  } catch (err) {
+    console.log(err);
+    handleAuthError(res);
+    return;
+  }
+  req.user = payload;
+  next();
+};
