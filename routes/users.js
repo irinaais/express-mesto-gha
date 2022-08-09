82eb08a37a8619187b07b847b50f6d270a33df2e
Iris <irinaais90@yandex.ru>
2022-08-10 00:07:57 +1000

    fix: auth

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ const {
 const { updateProfileValidator, updateAvatarValidator, idValidator } = require('../middlewares/validation');
 
 router.get('/', sendUsers);
-router.get('/:userId', idValidator, sendUserById);
 router.patch('/me', updateProfileValidator, updateUser);
 router.patch('/me/avatar', updateAvatarValidator, updateAvatar);
 router.get('/me', getMe);
+router.get('/:userId', idValidator, sendUserById);
 
 module.exports = router;
